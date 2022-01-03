8ae4dcd40591abc0b7dd73152cc1f0e41f6ba1cb
Focus Chen <i@focuschen.com>
2022-01-03 17:09:46 +0000

    try this

--- ykdl/get_stable.py
+++ ykdl/get_stable.py
@@ -1,5 +1,8 @@
 import urllib.request, json
 with urllib.request.urlopen("https://api.github.com/repos/SeaHOH/ykdl/releases/latest") as url:
     data = json.loads(url.read().decode())
-    print(data["zipball_url"])
-    urllib.request.urlretrieve(data["zipball_url"], "stable.zip")
+    print(data["tag_name"])
+    f = open("checkout.bat", "w")
+    f.write('@ECHO OFF')
+    f.write("git checkout tags/" + data["tag_name"])
+    f.close()
